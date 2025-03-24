7cbc554cff681ddcc126c3eb679eef806a512737
Arthur Ogunfuye <arthurity@yahoo.co.uk>
2025-03-24 01:18:56 +0000

    fix error

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,4 +1,6 @@
-export const Person = ({ name, age, sex, isMarried, partnerName }) => {
+export const Person = ({ person }) => {
+  const { name, age, sex, isMarried, partnerName } = person;
+
   return (
     <section className="Person">
       <h2 className="Person__name">My name is {name}</h2>
